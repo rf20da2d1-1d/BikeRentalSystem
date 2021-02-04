f014ec77f67dba98133c30d9b5050738b1eec229
Peter Levinsky <pele@easj.dk>
2021-02-04 11:53:40 +0100

    Resten af metoderne implementeret enkelte med alternative løsninger

--- BikeRentalSystem/BikeCatalog.cs
+++ BikeRentalSystem/BikeCatalog.cs
@@ -38,5 +38,57 @@ namespace BikeRentalSystem
             Console.WriteLine($"Antal cykler = {bikes.Count} og tidspunktet er {DateTime.Now}");
 
         }
+
+        public Bike SearchBike(String chassisNumber) // måtte tilpasse typen til propertien dvs int -> string
+        {
+            return bikes.Find(b => b.ChassisNumber == chassisNumber);
+        }
+
+        public Bike SearchBikeAlternativt(String chassisNumber) 
+        {
+            if (bikes.Exists(b => b.ChassisNumber == chassisNumber))
+            {
+                throw new KeyNotFoundException($"Cykel med stelnummer {chassisNumber} findes ikke");
+            }
+
+            return bikes.Find(b => b.ChassisNumber == chassisNumber);
+        }
+
+        public void DeleteBike(String chasisNumber)
+        {
+            Bike bikeToDelete = SearchBikeAlternativt(chasisNumber);
+            bikes.Remove(bikeToDelete);
+        }
+
+        public List<Bike> SearchBikesOfModel(string model)
+        {
+            List<Bike> foundBikes = new List<Bike>();
+            foreach (Bike bike in bikes)
+            {
+                if (bike.Model.Contains(model))
+                {
+                    foundBikes.Add(bike);
+                }
+            }
+
+            return foundBikes;
+        }
+
+        public List<Bike> SearchBikesOfModelAlternativt(string model)
+        {
+            return bikes.FindAll( b => b.Model.Contains(model));
+        }
+
+        public void deleteAllBikesOfModel(string model)
+        {
+            /*
+             * Man kan ikke slette i den liste du er ved at gennemløbe med foreach
+             * Du har i forrige opgave lavet en metoder der finder alle bike af en speciel model
+             */
+            foreach (Bike bike in SearchBikesOfModel(model))
+            {
+                bikes.Remove(bike);
+            }
+        }
     }
 }
